dec63aa42a2e05a9637f2958ba1e3bab7282870f
freddyskull <freddyskull11@gmail.com>
2024-11-25 16:58:53 -0400

    💄 ui: creando la variante hoverOutline

--- src/components/ui/badge.jsx
+++ src/components/ui/badge.jsx
@@ -1,34 +1,35 @@
-import * as React from "react"
-import { cva } from "class-variance-authority";
+import * as React from 'react'
+import { cva } from 'class-variance-authority'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
-      },
+          'bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        outline: 'text-foreground dark:border-accent border-slate-300',
+        hoverOutline: 'text-foreground dark:border-accent border-slate-300 cursor-pointer hover:bg-slate-500 hover:text-white'
+      }
     },
     defaultVariants: {
-      variant: "default",
-    },
+      variant: 'default'
+    }
   }
 )
 
-function Badge({
+function Badge ({
   className,
   variant,
   ...props
 }) {
-  return (<div className={cn(badgeVariants({ variant }), className)} {...props} />);
+  return (<div className={cn(badgeVariants({ variant }), className)} {...props} />)
 }
 
 export { Badge, badgeVariants }
